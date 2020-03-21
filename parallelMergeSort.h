6d86886230cd523f26856b29ac42e81c2ecb7ec8
Richard Kell <rakqhn@mst.edu>
2020-03-20 21:38:27 -0500

    added function documentation

--- parallelMergeSort.h
+++ parallelMergeSort.h
@@ -13,9 +13,32 @@
 
 using std::thread;
 
+/**
+ * \brief parallelization of the merge sort algorithm that uses multiple threads to sort concurrently
+ * \pre
+ *    * \p arrayOfNumbers is filled with unsorted longs 
+ *    * \p leftIndex and \p rightIndex are within the bounds of \p arrayOfNumbers 
+ * \post
+ *    * \p arrayOfNumbers is sorted
+ * \param arrayOfNumbers an array of type long
+ * \param leftIndex the starting index of the 
+ * \param rightIndex
+ * \param maxThreads the maximum number of threads parallelMergeSort is allowed to spawn
+ * \return none
+ */
 void parallelMergeSort(long* arrayOfNumbers, const unsigned long leftIndex, 
                             const unsigned long rightIndex, const unsigned long maxThreads = 0);
 
+/**
+ * \brief attempts to determine an efficient number of threads based on 
+ * the size of the array that will be sorted in parallelMergeSort()
+ * \pre
+ *    * none
+ * \post
+ *    * none
+ * \param arraySize the size of the array to be sorted in parallelMergeSort()
+ * \return an unsigned long - the maximum number of threads recommended to be used in parallelMergeSort()
+ */
 unsigned long determineMaxThreads(const unsigned long arraySize);
 
 #endif
