29ccb07be2c61f1e4fffed8645057580be7eae12
Richard Kell <rakqhn@mst.edu>
2020-03-20 19:55:37 -0500

    added function documentation

--- arrayFunctions.h
+++ arrayFunctions.h
@@ -13,10 +13,43 @@
 using std::cout;
 using std::endl;
 
-void fillTestArrays(long* emptyArray, const unsigned long size, const long minRandomNum, const long maxRandomNum);
+/**
+ * \brief takes an array of size \p size and fills it with random numbers between 
+ * \p minRandomNum and \p maxRandomNum inclusive
+ * \pre
+ *    * none
+ * \post
+ *    * \p array is filled with random numbers
+ * \param array an array of type long
+ * \param size the size of \p array
+ * \param minRandomNum the lowest random number to be generated when filling \p array
+ * \param maxRandomNum the highest random number to be generated when filling \p array
+ * \return none
+ */
+void fillArray(long* array, const unsigned long size, const long minRandomNum, const long maxRandomNum);
 
-void printArray(long* emptyArray, const unsigned long size);
+/**
+ * \brief prints the contents of \p array to the console
+ * \pre
+ *    * none
+ * \post
+ *    * none
+ * \param array an array of type long
+ * \param size the size of \p array
+ * \return none
+ */
+void printArray(long* array, const unsigned long size);
 
+/**
+ * \brief checks if \p array is a an array of sorted longs
+ * \pre
+ *    * none
+ * \post
+ *    * none
+ * \param array an array of type long
+ * \param size the size of \p array
+ * \return a boolean that is true if \p array is sorted, false if it is unsorted
+ */
 bool arrayIsSorted(long* array, const unsigned long size);
 
 #endif
